c4077b7ad149ec2f83b228c642c73227ba22d650
langedev <145938864+miangedev@users.noreply.github.com>
2025-05-13 09:24:10 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,9 +11,6 @@ window.addEventListener('scroll',()=>{
     }
 })
 
-/*=========================================
-    Tabs
-==========================================*/
 if(document.querySelector('.hm-tabs')){
 
     const tabLinks=document.querySelectorAll('.hm-tab-link');
